5b4040e5c6f5873e5124d396b4ba239a6c49ce91
Chevyosa <riyandaazis00@gmail.com>
2023-09-28 22:18:56 +0700

    First Commit - Create Basic Website
    - Create Some Basic HTML
    - Create Some CSS
    - Added Some Pictures Assets
    - Added Few PHP for localhost
    
    Co-Authored-By: ritsu <103413173+Sutrii@users.noreply.github.com>
    Co-Authored-By: kambingcute <119449420+kambingcute@users.noreply.github.com>
    Co-Authored-By: semidust <117661820+semidust@users.noreply.github.com>

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php 
+
+$server = "localhost";
+$user = "root";
+$pass = "";
+$database = "patient_check";
+
+$conn = mysqli_connect($server, $user, $pass, $database);
+
+if (!$conn) {
+    die("<script>alert('Failed to Connect with Database.')</script>");
+}
+
+?>
