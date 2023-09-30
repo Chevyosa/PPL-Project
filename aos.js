d84884a8d6342565af36155143926f79b01145eb
Chevyosa <riyandaazis00@gmail.com>
2023-09-30 13:52:06 +0700

    Update about-us.html page & add new JS Script
    - Updated About Us page
    - Added new JS Script
    
    Co-Authored-By: ritsu <103413173+Sutrii@users.noreply.github.com>

---
+++ aos.js
@@ -0,0 +1,22 @@
+//===== Animation On Scroll =====//
+AOS.init();
+
+// //===== Navbar Alert =====//
+// function navhome()
+// {
+//     window.alert("Welcome to the Homepage!");
+// }
+// function navprofile()
+// {
+//     window.alert("You will be visiting the Developer Profile...");
+// }
+
+// //===== Button More Aesthetic =====//
+// function clickmore()
+// {
+//     window.alert("You will be visiting more aesthetic page... ");
+// }
+// function buttonmore()
+// {
+//     window.alert("Find more your aesthetic here!");
+// }
